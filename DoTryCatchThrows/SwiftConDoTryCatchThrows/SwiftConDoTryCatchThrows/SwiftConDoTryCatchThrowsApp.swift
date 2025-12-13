28fdf71be1210a850efff5433231a3e1b517d2d1
Nikunj <nikunjmodi86@gmail.com>
2025-12-13 21:36:49 +0530

    Add DoTryCatchThrows module

---
+++ DoTryCatchThrows/SwiftConDoTryCatchThrows/SwiftConDoTryCatchThrows/SwiftConDoTryCatchThrowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConDoTryCatchThrowsApp.swift
+//  SwiftConDoTryCatchThrows
+//
+//  Created by Nikunj Modi on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConDoTryCatchThrowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
